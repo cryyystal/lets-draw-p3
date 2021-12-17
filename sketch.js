d87fdc2408178d453209d740863ac49ba8fc7c34
Crystal Wong <crystalwong-cw@hotmail.com>
2021-12-17 05:02:24 -0500

    colour buttons created+
    button naming changed, added button for red, trying to get red button clickable and change brush to a red colour

--- sketch.js
+++ sketch.js
@@ -1,76 +1,92 @@
-
-
 var hue;
 var rainbow = true;
 var rate = 1;
 let button;
+let img;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("white");
   hue = 0;
 
-  button = createButton("Export My Masterpiece");
-  button.mousePressed(masterpiece);
+  button = createButton("Export My Art");
+  button.mousePressed(art);
+
+  button = createButton("Red");
+  button.mousePressed(red);
+
+  // button = createButton("Orange");
+  // button.mouseClicked(orange);
 
 }
 
-function masterpiece(){
+function art(){
   noLoop();
-  save("My-Masterpiece");
+  save("My-Art");
 }
 
-
-
-
-
-function draw() {
-
-
+function red(){
+  colorMode(HSL, 360);
+  noStroke();
+  fill(0, 200, 200);
+  ellipse(mouseX, mouseY, 50, 50);
 }
 
+// function orange(){
+//   if (mouseIsPressed === true) {
+//   colorMode(hsl,);
+//   noStroke();
+//   fill();
+//   ellipse(mouseX, mouseY, 25, 25);
+// }
+
+// function draw() {
 
 
-
-
-
-
-
+// }
 
 
 
 function mouseDragged() {
-  if (rainbow) {
-    if (hue > 360) {
-        hue = 0;
-    } else {
-        hue += rate;
-    }
-  }
+  // if (rainbow) {
+  //   if (hue > 360) {
+  //       hue = 0;
+  //   } else {
+  //       hue += rate;
+  //   }
+  // }
+
   colorMode(HSL, 360);
   noStroke();
-  fill(hue, 200, 200);
+  fill(0);
   ellipse(mouseX, mouseY, 25, 25);
+
+  if (mouseIsPressed === true) {
+    colorMode(HSL, 360);
+    noStroke();
+    fill(0);
+    ellipse(mouseX, mouseY, 25, 25);
+  }
 }
 
-function keyPressed() {
-  if (keyCode == 82) {
-    hue = 0;
-    rainbow = false;
-  }
-  if (keyCode == 71) {
-    hue = 125;
-    rainbow = false;
-  }
-  if (keyCode == 66) {
-    hue = 200;
-    rainbow = false;
-  }
-  if (keyCode == 32) {
-    rainbow = true;
-    rate *= 2;
-  }
-}
+// function keyPressed() {
+//   if (keyCode == 82) {
+//     hue = 0;
+//     rainbow = false;
+//   }
+//   if (keyCode == 71) {
+//     hue = 125;
+//     rainbow = false;
+//   }
+//   if (keyCode == 66) {
+//     hue = 200;
+//     rainbow = false;
+//   }
+//   if (keyCode == 32) {
+//     rainbow = true;
+//     rate *= 2;
+//   }
+// }
 
 
 
